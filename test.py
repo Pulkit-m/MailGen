63bc043f646a953b5447a5bd23630afbe30c5fc5
Pulkit Mahajan <2019ucs0073@iitjammu.ac.in>
2021-02-08 23:41:34 +0530

    Python Script initialized to read data directly from google spreadsheet

---
+++ test.py
@@ -0,0 +1,19 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+import pandas as pd
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json' , scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("sample_sheet").sheet1
+data = sheet.get_all_records()
+
+df = pd.DataFrame(data)
+print(df.head(9),'\n','\n')
+
+print(df[df['email sent or not']==0]['Email Id'].unique())
+
+pprint(df)
